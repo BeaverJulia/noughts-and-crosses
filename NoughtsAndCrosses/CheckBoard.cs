9e3db4606725685c3b4e1b3016cc2ba8099d980c
Julia Gula <julia.gula@openwrks.com>
2021-11-22 19:07:48 +0000

    #changed tests

--- NoughtsAndCrosses/CheckBoard.cs
+++ NoughtsAndCrosses/CheckBoard.cs
@@ -6,7 +6,7 @@
         {
             if ((board[1] == board[2] && board[2] == board[3]) || //Horizontal1
                 (board[4] == board[5] && board[5] == board[6]) || //Horizontal2
-                (board[6] == board[7] && board[7] == board[8]) || //Horizontal3
+                (board[8] == board[7] && board[7] == board[8]) || //Horizontal3
                 (board[1] == board[4] && board[4] == board[7]) || //Vertical1
                 (board[2] == board[5] && board[5] == board[8]) || //Vertical2
                 (board[3] == board[6] && board[6] == board[9]) || //Vertical3
